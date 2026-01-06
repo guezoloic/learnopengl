f4f9f4a7ac56e4cd346be3597864a7fc785e06a0
Loïc GUEZO <loicguezo@gmail.com>
2026-01-06 20:06:25 +0100

    feat(core.hpp): add central import file

---
+++ inc/core/core.hpp
@@ -0,0 +1,24 @@
+#ifndef CORE_HPP
+#define CORE_HPP
+
+#include "glad/glad.h"
+
+#include "GLFW/glfw3.h"
+
+#include "glm/glm.hpp"
+
+#include "stb_image.h"
+
+#include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
+
+#include "logger.hpp"
+#include "camera.hpp"
+#include "vao.hpp"
+#include "vbo.hpp"
+#include "ebo.hpp"
+#include "shader.hpp"
+#include "time.hpp"
+
+#endif
